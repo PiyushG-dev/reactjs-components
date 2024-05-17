914bbcff4350c26065eca6a04851de1cecbb4173
Piyush Goswami <pjgpiyush@gmail.com>
2024-05-17 22:53:27 +0530

    hover states for polaroid

--- components/ui/polaroid.jsx
+++ components/ui/polaroid.jsx
@@ -3,8 +3,20 @@ import Image from "next/image";
 
 const Polaroid = ({ img }) => {
   return (
-    <div className="h-full pb-16 bg-white sm:h-[450px]">
+    <div className="polaroid-container relative h-full pb-16 bg-white sm:h-[450px]">
       <Image src={img} alt="story" className="h-full object-cover px-3 pt-3" />
+      <div className="overlay">
+        <div className="font-Marcellus text-lg px-6">
+          <p>
+            Pragya and Adil’s story was no other than a match made in heaven. An
+            intimate journey immersed with love and laughter with family and
+            friends from far and near.
+          </p>
+        </div>
+        <p className="date text-xl text-quaternary font-Marcellus absolute bottom-4">
+          Jan 2024
+        </p>
+      </div>
     </div>
   );
 };
--- components/ui/portfolio-card.jsx
+++ components/ui/portfolio-card.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Polaroid from "./polaroid";
+import Arrow from "../illustrations/arrow";
 
 const PortfolioCard = ({ img, title }) => {
   return (
-    <div className="flex flex-col gap-5 lg:gap-2">
+    <div className="flex flex-col gap-5 lg:gap-2 portfolio-card-container">
       <Polaroid img={img} />
       <div className="font-Marcellus">
         <h3 className="text-xl lg:text-lg">{title}</h3>
-        <p className="lg:text-sm">Read the story</p>
+        <p className="lg:text-sm relative w-fit flex items-center gap-1">
+          Read the story
+          <Arrow />
+          <span className="read"></span>
+        </p>
       </div>
     </div>
   );
